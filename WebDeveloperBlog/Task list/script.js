5fd1c40d86b846fdd7625a86e8c495316b2ccaf4
Fedor Kosolapov <kosolapov.f@nekidaem.ru>
2019-10-29 10:27:08 +0700

    WebDeveloperBlog Урок task list

---
+++ WebDeveloperBlog/Task list/script.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: "Список задач",
+        items: [
+            { text: 'First'},
+            { text: 'Second'},
+            { text: 'Third'},
+            { text: 'Fourth'},
+        ]
+    },
+    methods:{
+        addItem: function(){
+            var input = document.getElementById('itemForm');
+
+            if (input.value !== ''){
+                this.items.push({
+                    text: input.value
+                })
+                input.value = ""
+            }
+        },
+
+        deleteItem: function(index){
+            this.items.splice(index, 1);
+        }
+    }
+})
