c0bc9628dc250a921930f4da1dd1571396f03aff
Fedor Kosolapov <kosolapov.f@nekidaem.ru>
2019-10-29 15:10:18 +0700

    WebDeveloperBlog Адаптивное меню

---
+++ WebDeveloperBlog/Адаптивное меню/script.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: [
+            'Home',
+            'About',
+            'Contacts',
+            'Blog',
+            'Map',
+        ],
+        show: false,
+    },
+});
