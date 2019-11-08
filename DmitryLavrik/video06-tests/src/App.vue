00c073001f59403f630184c31379fec92ac2ee79
Fedor Kosolapov <kosolapov.f@nekidaem.ru>
2019-11-08 15:16:35 +0700

    Начал задание из видео 6 (тесты)

---
+++ DmitryLavrik/video06-tests/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <results
+      :questions="questions"
+      :answers="answers"
+    ></results>
+  </div>
+</template>
+
+<script>
+
+import results from './components/Results';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      questions: [
+        'Сколько будет 2х2?',
+        'Сколько будет 3х5?',
+      ],
+      answers: [
+        'Это будет 4',
+        'А это 15',
+      ],
+    }
+  },
+  components: {
+    results
+  },
+}
+
+function getQuests(){
+  return [
+    {
+      type: 'radio',
+      title: 'Какой тег задаёт ссылку?',
+      answers: [
+        'a', 'div', 'span', 'img',
+      ],
+    },
+    {
+      type: 'checkbox',
+      title: 'Какие из тегов строчные?',
+      answers: [
+        'a', 'div', 'span', 'img',
+      ],
+    },
+  ];
+}
+
+</script>
+
---
+++ DmitryLavrik/video06-tests/src/components/Results.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Вопрос</th>
+                    <th>Ваш ответ</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(qwestion, index) in questions" :key="index">
+                    <td>{{ qwestion }}</td>
+                    <td>{{ answers[index] }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        questions: Array,
+        answers: Array,
+    }
+}
+</script>
